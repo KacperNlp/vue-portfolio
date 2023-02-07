06951e7425a4ee6e2c2f0b5670cd66fcf1585d66
Kacper Nalepa <kacper.nlp@wp.pl>
2023-02-07 17:24:11 +0100

    Created BaseHeadline

---
+++ src/components/base/BaseHeadline.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="base-headline">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "BaseHeadline",
+};
+</script>
+<style lang="scss" scoped>
+.base-headline {
+  h1,
+  h2,
+  h3 {
+    font-weight: 700;
+    color: #d9d9d9;
+  }
+
+  h1 {
+    font-size: 64px;
+
+    @media (min-width: 768px) {
+      font-size: 96px;
+    }
+  }
+
+  h2 {
+    font-size: 48px;
+
+    @media (min-width: 768px) {
+      font-size: 64px;
+    }
+  }
+
+  h3 {
+    font-size: 24px;
+
+    @media (min-width: 768px) {
+      font-size: 32px;
+    }
+  }
+}
+</style>
