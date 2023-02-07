bb602d0fdf00b6df715947ff3958650a6998d740
Kacper Nalepa <kacper.nlp@wp.pl>
2023-02-07 17:19:02 +0100

    Created TheHeader, TheFooter and BaseBar

---
+++ src/components/base/BaseBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <button class="bar" :class="{ active: isNavActive }" @click="openNav">
+    <span></span>
+  </button>
+</template>
+<script>
+export default {
+  name: "BaseBar",
+  data() {
+    return {
+      isNavActive: false,
+    };
+  },
+
+  methods: {
+    openNav() {
+      this.isNavActive = !this.isNavActive;
+    },
+  },
+};
+</script>
+<style lang="scss">
+.bar {
+  position: fixed;
+  top: 25px;
+  right: 20px;
+  height: 27px;
+  width: 34px;
+  z-index: 99;
+  background: transparent;
+  border: none;
+  outline: transparent;
+  overflow: hidden;
+
+  span {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    width: 100%;
+    height: 1px;
+    background: #d9d9d9;
+    transition: 0.3s;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: #d9d9d9;
+    transition: 0.3s;
+    transform-origin: center left;
+  }
+
+  &::before {
+    top: 0;
+  }
+
+  &::after {
+    top: calc(100% - 1px);
+  }
+
+  &.active {
+    span {
+      left: -105%;
+    }
+
+    &::before {
+      transform: rotate(45deg) translate(2px, 1px);
+    }
+
+    &::after {
+      transform: rotate(-45deg) translate(1px, 1px);
+    }
+  }
+
+  &:hover {
+    span,
+    &::before,
+    &::after {
+      background: #cd5888;
+    }
+  }
+}
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <base-bar></base-bar>
+  </header>
+</template>
+<script>
+import BaseBar from "../base/BaseBar.vue";
+
+export default {
+  name: "TheHeader",
+  components: { BaseBar },
+};
+</script>
+<style lang="scss"></style>
